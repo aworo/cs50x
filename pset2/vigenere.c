0c06ec667ddc889f144971f56c80929777694c43
Aaron W <yukoncountonit@gmail.com>
2014-10-05 17:54:17 -0400

    Reused code from caesar.c as template for vignere.c

--- pset2/vigenere.c
+++ pset2/vigenere.c
@@ -7,15 +7,26 @@
 
 int main(int argc, string argv[])
 {
-
-    if (( argc != 2  ) || ( atoi(argv[1]) < 0 ))
+    // string k = argv[1];   // k is the keyword entered as a CL argument
+    if ( argc != 2  ) // Need to add condition for non-alpha check
     {
-        printf("You should only have one argument!\n");
+        printf("You should only have one alphaetical argument!\n");
         return 1;
     }
     else
     {
-    
-    
+        string p = GetString();  //Prompt user for plaintext
+        for (int i = 0, n = strlen(p); i < n; i++)
+        {
+            if ( isalpha( p[i] ) )
+            {
+            
+            }
+            
+            else 
+            {
+                printf("%c", p[i]);
+            }
+        }    
     }
 }

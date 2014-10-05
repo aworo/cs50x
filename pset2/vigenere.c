8643f860424492f19811624a88971fa0388cc846
Aaron W <yukoncountonit@gmail.com>
2014-10-05 18:53:00 -0400

    vignere partially works, lots of work to be done

--- pset2/vigenere.c
+++ pset2/vigenere.c
@@ -7,7 +7,9 @@
 
 int main(int argc, string argv[])
 {
-    // string k = argv[1];   // k is the keyword entered as a CL argument
+
+
+    string k = argv[1];   // k is the keyword entered as a CL argument
     if ( argc != 2  ) // Need to add condition for non-alpha check
     {
         printf("You should only have one alphaetical argument!\n");
@@ -16,11 +18,30 @@ int main(int argc, string argv[])
     else
     {
         string p = GetString();  //Prompt user for plaintext
+        int keychar = 0;
         for (int i = 0, n = strlen(p); i < n; i++)
         {
-            if ( isalpha( p[i] ) )
+            if ( isalpha( p[i] ) )  
             {
-            
+                if ( isupper( p[i] ) )
+
+                {
+                    if ((k[keychar % strlen(k)] + p[i]- 97) > 132 )
+                    {
+                        printf("%c", p[i] + k[keychar % strlen(k)] - 71);
+                    }
+                    else
+                    {
+                        printf("%c", p[i] + k[keychar % strlen(k)] - 97);
+                    }    
+                    keychar++;                    
+                }
+                    
+                else
+                {
+                    printf("%c", p[i] + k[keychar % strlen(k)] - 97);
+                    keychar++;
+                }
             }
             
             else 

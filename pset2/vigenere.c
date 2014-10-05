5e3b71b59e17e94fe28d52b15afe08b394141a9d
Aaron W <yukoncountonit@gmail.com>
2014-10-05 17:44:17 -0400

    Started vignere.c

---
+++ pset2/vigenere.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(int argc, string argv[])
+{
+
+    if (( argc != 2  ) || ( atoi(argv[1]) < 0 ))
+    {
+        printf("You should only have one argument!\n");
+        return 1;
+    }
+    else
+    {
+    
+    
+    }
+}
